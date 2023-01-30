c9de70007769e6d2fda2b2c91e4da7e2080675af
hatlonely <hatlonely@gmail.com>
2023-01-30 18:25:45 +0800

    update

---
+++ aws/Lambda/code/aws-lambda-base-image-python/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "event": event,
+        })
+    }
