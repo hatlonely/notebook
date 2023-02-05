3002a8043eb395dacfa908451e611d1a2098d125
hatlonely <hatlonely@gmail.com>
2023-02-06 00:26:45 +0800

    update

--- 云原生/terraform/code/backend-oss/main.tf
+++ 云原生/terraform/code/backend-oss/main.tf
@@ -22,9 +22,35 @@ provider "alicloud" {
 }
 
 resource "alicloud_oss_bucket" "oss-tf-state" {
-  bucket = "${name}-tf-state"
+  bucket = "${var.name}-tf-state"
 }
 
 resource "alicloud_ots_instance" "ots-tf-remote" {
-  name = "${name}-tf-remote"
+  name = "${var.name}-tf-remote"
+}
+
+resource "alicloud_ots_table" "ots-tf-remote-table" {
+  instance_name = alicloud_ots_instance.ots-tf-remote.name
+  max_version   = 1
+  table_name    = "statelock"
+  time_to_live  = -1
+  primary_key {
+    name = "LockID"
+    type = "String"
+  }
+}
+
+output "description" {
+  value = <<EOT
+terraform {
+  backend "oss" {
+    bucket              = "${alicloud_oss_bucket.oss-tf-state.bucket}"
+    prefix              = ""
+    key                 = ""
+    region              = "${var.region}"
+    tablestore_endpoint = "https://${alicloud_ots_instance.ots-tf-remote.name}.${var.region}.ots.aliyuncs.com"
+    tablestore_table    = "${alicloud_ots_table.ots-tf-remote-table.table_name}"
+  }
+}
+EOT
 }
