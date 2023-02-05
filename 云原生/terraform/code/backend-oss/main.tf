24e1a5f07b672d843b1ae9d5a6877f9af8beb7ad
hatlonely <hatlonely@gmail.com>
2023-02-06 00:09:28 +0800

    update

--- 云原生/terraform/code/backend-oss/main.tf
+++ 云原生/terraform/code/backend-oss/main.tf
@@ -21,10 +21,10 @@ provider "alicloud" {
   region     = var.region
 }
 
-resource "alicloud_oss_bucket" "oss-bucket" {
+resource "alicloud_oss_bucket" "oss-tf-state" {
   bucket = "${name}-tf-state"
 }
 
-resource "alicloud_ots_instance" "ots-instance" {
+resource "alicloud_ots_instance" "ots-tf-remote" {
   name = "${name}-tf-remote"
 }
