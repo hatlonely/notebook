3bce9ff6b7ce5234dbe3559d878d688aa5275b9b
hatlonely <hatlonely@gmail.com>
2023-06-29 01:22:25 +0800

    update

---
+++ python/pytest/code/test-acs/case/test_case1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+def test_case1():
+    print("test_case1")
+    assert 1 == 1
---
+++ python/pytest/code/test-acs/envs/env.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+class Environment:
+    def __init__(self):
+        self.client1 = "client1"
+        self.client2 = "client2"
