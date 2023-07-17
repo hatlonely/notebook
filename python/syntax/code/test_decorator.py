64ce02ec7e1360c8e4eed625a64860029ab21fe9
hatlonely <hatlonely@gmail.com>
2023-07-17 15:30:18 +0800

    update

--- python/syntax/code/test_decorator.py
+++ python/syntax/code/test_decorator.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import json
 import time
+
+import yaml
 
 
 def simple_decorator(func):
@@ -12,6 +15,7 @@ def simple_decorator(func):
     return wrapper
 
 
+# 装饰方法
 def test_simple_decorator():
     def hello():
         print('Hello world')
@@ -21,6 +25,7 @@ def test_simple_decorator():
     print('Done')
 
 
+# 简单装饰器
 def test_simple_decorator2():
     @simple_decorator
     def hello():
@@ -45,6 +50,7 @@ def retry(times=3, delay=0.1):
     return decorator
 
 
+# 带参数的装饰器
 def test_retry():
     @retry(times=5, delay=0.05)
     def hello():
@@ -52,3 +58,148 @@ def test_retry():
         raise Exception('Timeout')
 
     hello()
+
+
+# 装饰类
+def log_decorator(cls):
+    def caller_with_log(caller):
+        def call(*args, **kwargs):
+            print(f"{caller.__name__}: call start")
+            res = caller(*args, **kwargs)
+            print(f"{caller.__name__}: call end")
+            return res
+
+        return call
+
+    for attr in cls.__dict__:
+        caller = getattr(cls, attr)
+        if callable(caller):
+            if caller.__name__.startswith("__"):
+                continue
+            setattr(cls, attr, caller_with_log(getattr(cls, attr)))
+    return cls
+
+
+def test_class_decorator():
+    @log_decorator
+    class A:
+        def func1(self):
+            print("func1 call")
+
+        def func2(self):
+            print("func1 call")
+
+    a = A()
+    a.func1()
+    a.func2()
+
+
+def json_object(cls):
+    def _init_from_dict__(self, d: dict):
+        for field, field_type in cls.__annotations__.items():
+            # 跳过不存在的字段
+            if field not in d:
+                continue
+            # 列表类型
+            if hasattr(field_type, "__class_getitem__") and field_type.__origin__ is list and field_type.__args__:
+                eles = field_type()
+                for e in d.get(field, []):
+                    eles.append(field_type.__args__[0](e))
+                setattr(self, field, eles)
+            # 字典类型
+            else:
+                setattr(self, field, field_type(d.get(field)))
+
+    def _init__(self, s, type_="json"):
+        t = type(s)
+        if t is str or t is bytes:
+            if type_ == "json":
+                s = json.loads(s)
+            if type_ == "yaml":
+                s = yaml.safe_load(s)
+        _init_from_dict__(self, s)
+
+    def _str__(self):
+        return json.dumps(vars(self), default=lambda x: vars(x), indent=2)
+
+    setattr(cls, "__init__", _init__)
+    setattr(cls, "__str__", _str__)
+
+    return cls
+
+
+def test_json():
+    @json_object
+    class A():
+        key1: int
+        key2: str
+
+        @json_object
+        class B:
+            key3: str
+
+            @json_object
+            class C:
+                key4: str
+
+            c: C
+            cs: list[C]
+
+        b: B
+        bs: list[B]
+
+    a = A({
+        "key1": "1",
+        "key2": "val2",
+        "b": {
+            "key3": "val3",
+            "c": {
+                "key4": "val4"
+            },
+            "cs": [{
+                "key4": "val4"
+            }]
+        },
+        "bs": [{
+            "key3": "val3",
+            "c": {
+                "key4": "val4"
+            },
+            "cs": [{
+                "key4": "val4"
+            }]
+        }]
+    })
+    print(a)
+    assert a.key1 == 1
+    assert a.key2 == "val2"
+    assert a.b.cs[0].key4 == "val4"
+    assert a.bs[0].cs[0].key4 == "val4"
+
+    a = A("""{
+        "key1": 1,
+        "key2": "val2",
+        "b": {
+            "key3": "val3",
+            "c": {
+                "key4": "val4"
+            },
+            "cs": [{
+                "key4": "val4"
+            }]
+        },
+        "bs": [{
+            "key3": "val3",
+            "c": {
+                "key4": "val4"
+            },
+            "cs": [{
+                "key4": "val4"
+            }]
+        }]
+    }""")
+    print(a)
+    assert a.key1 == 1
+    assert a.key2 == "val2"
+    assert a.b.cs[0].key4 == "val4"
+    assert a.bs[0].cs[0].key4 == "val4"
