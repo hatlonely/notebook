86eeddc976bbaf8679032b24a0591a5e880821bc
hatlonely <hatlonely@gmail.com>
2023-08-22 23:27:02 +0800

    update

--- terraform/code/alicloud/best-practise/1-ecs-web-service/ecs.tf
+++ terraform/code/alicloud/best-practise/1-ecs-web-service/ecs.tf
@@ -3,6 +3,11 @@ data "alicloud_instance_types" "web-instance-types" {
   memory_size    = 1
 }
 
+data "alicloud_instance_types" "jump-server-instance-types" {
+  cpu_core_count = 1
+  memory_size    = 1
+}
+
 data "alicloud_images" "ubuntu_22" {
   name_regex = "^ubuntu_22"
   owners     = "system"
@@ -50,3 +55,40 @@ resource "alicloud_security_group_rule" "tf-test-security-group-rule" {
 #    ]
 #  }
 #}
+
+resource "tls_private_key" "tf-test-key-pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "alicloud_ecs_key_pair" "tf-test-key-pair" {
+  key_pair_name = "tf-test-key-pair"
+  public_key    = tls_private_key.tf-test-key-pair.public_key_openssh
+}
+
+resource "alicloud_instance" "tf-test-jump-server" {
+  image_id        = data.alicloud_images.ubuntu_22.images[0].id
+  instance_type   = data.alicloud_instance_types.jump-server-instance-types.instance_types[0].id
+  security_groups = [
+    alicloud_security_group.tf-test-security-group.id,
+  ]
+  vswitch_id                 = alicloud_vswitch.tf-test-vswitch[0].id
+  internet_max_bandwidth_out = 5
+  internet_charge_type       = "PayByTraffic"
+  host_name                  = "tf-test-ecs"
+  key_name                   = alicloud_ecs_key_pair.tf-test-key-pair.key_pair_name
+}
+
+resource "local_file" "tf-test-id-rsa" {
+  filename = "id_rsa"
+  content  = tls_private_key.tf-test-key-pair.private_key_pem
+}
+
+resource "local_file" "tf-test-id-rsa-pub" {
+  filename = "id_rsa.pub"
+  content  = tls_private_key.tf-test-key-pair.public_key_openssh
+}
+
+output "connection-jump-server" {
+  value = "ssh -i id_rsa root@${alicloud_instance.tf-test-jump-server.public_ip}"
+}
--- terraform/code/alicloud/best-practise/1-ecs-web-service/main.tf
+++ terraform/code/alicloud/best-practise/1-ecs-web-service/main.tf
@@ -9,6 +9,16 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
   }
 }
 
