61fcf5f8afd762b4b84c8e048a08d5caf51b442f
hatlonely <hatlonely@gmail.com>
2023-08-24 00:21:25 +0800

    Update ec2.tf

--- terraform/code/aws/ec2/ec2.tf
+++ terraform/code/aws/ec2/ec2.tf
@@ -16,13 +16,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 data "aws_ami" "ubuntu_22" {
   owners      = ["amazon"]
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*22.04*-amd64-server-20230728"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
   }
+}
+
+# 创建 vpc
+resource "aws_vpc" "tf-test-vpc" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_subnet" "tf-test-subnet" {
+  vpc_id            = aws_vpc.tf-test-vpc.id
+  availability_zone = "us-east-1a"
+  cidr_block        = "172.31.1.0/24"
 }
 
 # 生成登录秘钥对
@@ -48,5 +60,37 @@ resource "aws_key_pair" "tf-test-key-pair" {
   public_key = tls_private_key.tf-test-key-pair.public_key_openssh
 }
 
+# 创建安全组
+resource "aws_security_group" "tf-test-security-group" {
+  name   = "tf-test-security-group"
+  vpc_id = aws_vpc.tf-test-vpc.id
+  ingress {
+    description      = "ssh"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 
+resource "aws_instance" "tf-test-instance" {
+  ami                         = data.aws_ami.ubuntu_22.id
+  instance_type               = "t3.nano"
+  key_name                    = aws_key_pair.tf-test-key-pair.key_name
+  security_groups             = [aws_security_group.tf-test-security-group.id]
+  subnet_id                   = aws_subnet.tf-test-subnet.id
+  associate_public_ip_address = true
+}
+
+output "connection" {
+  value = "ssh -i id_rsa ubuntu@${aws_instance.tf-test-instance.public_ip}"
+}
