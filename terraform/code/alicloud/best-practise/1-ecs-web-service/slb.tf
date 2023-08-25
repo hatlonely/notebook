5959db376014de5676c1c3a0db92bbf14633ab3f
hatlonely <hatlonely@gmail.com>
2023-08-26 01:37:50 +0800

    Update slb.tf

---
+++ terraform/code/alicloud/best-practise/1-ecs-web-service/slb.tf
@@ -0,0 +1,33 @@
+resource "alicloud_slb_load_balancer" "tf-test-load-balancer" {
+  load_balancer_name   = "tf-test-load-balancer"
+  address_type         = "internet"
+  load_balancer_spec   = "slb.s1.small"
+  internet_charge_type = "PayByTraffic"
+}
+
+resource "alicloud_slb_server_group" "tf-test-server-group" {
+  name             = "tf-test-server-group"
+  load_balancer_id = alicloud_slb_load_balancer.tf-test-load-balancer.id
+}
+
+resource "alicloud_slb_server_group_server_attachment" "tf-test-server-group-server-attachment" {
+  count           = var.instance_number
+  server_group_id = alicloud_slb_server_group.tf-test-server-group.id
+  server_id       = alicloud_instance.tf-test-web-service[count.index].id
+  port            = 80
+}
+
+resource "alicloud_slb_listener" "tf-test-listener" {
+  load_balancer_id = alicloud_slb_load_balancer.tf-test-load-balancer.id
+  backend_port     = 80
+  frontend_port    = 80
+  protocol         = "tcp"
+  bandwidth        = 1
+  server_group_id  = alicloud_slb_server_group.tf-test-server-group.id
+}
+
+output "slb_connection" {
+  value = <<EOF
+curl http://${alicloud_slb_load_balancer.tf-test-load-balancer.address}
+EOF
+}
