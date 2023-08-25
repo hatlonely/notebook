175236ef4620693a061d5abfc3201762d830efd8
hatlonely <hatlonely@gmail.com>
2023-08-26 00:01:24 +0800

    Update vpn.tf

--- terraform/code/aws/vpn/vpn.tf
+++ terraform/code/aws/vpn/vpn.tf
@@ -92,6 +92,15 @@ resource "aws_security_group" "tf-test-security-group" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "ssh"
+    from_port        = 12674
+    to_port          = 12674
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
