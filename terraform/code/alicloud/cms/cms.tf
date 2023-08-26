f37fd8065e7d4cb8f16598bcdfadfd426eff475b
hatlonely <hatlonely@gmail.com>
2023-08-27 00:31:34 +0800

    Update cms.tf

--- terraform/code/alicloud/cms/cms.tf
+++ terraform/code/alicloud/cms/cms.tf
@@ -6,3 +6,18 @@ terraform {
     }
   }
 }
+
+resource "alicloud_cms_alarm_contact" "hatlonely" {
+  alarm_contact_name = "hatlonely"
+  describe           = "hatlonely"
+  channels_mail      = "hatlonely@foxmail.com"
+  channels_sms       = "13112341234"
+  lang               = "zh-cn"
+}
+
+resource "alicloud_cms_alarm_contact_group" "tf-test-cms-alarm-contact-group" {
+  alarm_contact_group_name = "tf-test-cms-alarm-contact-group"
+  contacts                 = [
+    alicloud_cms_alarm_contact.hatlonely.id,
+  ]
+}
