b922817abb305035c23b4c508c0c57bbcd004924
hatlonely <hatlonely@gmail.com>
2023-08-27 14:05:08 +0800

    Update eci.tf

---
+++ terraform/code/alicloud/best-practise/2-eci-web-service/eci.tf
@@ -0,0 +1,39 @@
+# 创建安全组，现在安全组只能创建在 VPC 内，但其规则可以是公网或内网
+resource "alicloud_security_group" "tf-test-security-group" {
+  name   = "tf-test-security-group"
+  vpc_id = alicloud_vpc.tf-test-vpc.id
+}
+
+# 创建安全组规则，nic_type 必须为 intranet，但其规则对公网同样有效
+resource "alicloud_security_group_rule" "tf-test-security-group-rule" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.tf-test-security-group.id
+  cidr_ip           = "123.113.97.7/32"
+}
+
+resource "alicloud_eci_container_group" "tf-test-eci-container-group" {
+  count = var.instance_number
+
+  container_group_name = "tf-test-eci-container-group"
+  security_group_id    = alicloud_security_group.tf-test-security-group.id
+  vswitch_id           = alicloud_vswitch.tf-test-vswitch[count.index % length(alicloud_vswitch.tf-test-vswitch)].id
+  restart_policy       = "OnFailure"
+  cpu                  = 1
+  memory               = 2
+  auto_create_eip      = false
+
+  containers {
+    image             = "registry-vpc.cn-beijing.aliyuncs.com/eci_open/nginx"
+    name              = "nginx"
+    image_pull_policy = "IfNotPresent"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
