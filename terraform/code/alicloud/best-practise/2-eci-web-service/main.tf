a3beeed6b8b74783abac8a2344f7c8bf0a87ae7d
hatlonely <hatlonely@gmail.com>
2023-08-27 13:59:40 +0800

    Update main.tf

---
+++ terraform/code/alicloud/best-practise/2-eci-web-service/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "1.209.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "alicloud" {
+  region = "cn-beijing"
+  alias  = "cn-beijing"
+}
+
+variable "instance_number" {
+  type    = number
+  default = 1
+}
