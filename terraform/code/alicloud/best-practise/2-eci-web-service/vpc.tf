3e00325eed8a464854e5cc5a91bf0efa4248742b
hatlonely <hatlonely@gmail.com>
2023-08-27 14:09:05 +0800

    Update vpc.tf

--- terraform/code/alicloud/best-practise/2-eci-web-service/vpc.tf
+++ terraform/code/alicloud/best-practise/2-eci-web-service/vpc.tf
@@ -25,7 +25,7 @@ resource "alicloud_vswitch" "tf-test-vswitch" {
     for idx, zone in local.ecs_available_zone_cn_beijing : idx => zone
   }
 
-  zone_id    = each.value.id
+  zone_id    = each.value
   vpc_id     = alicloud_vpc.tf-test-vpc.id
   cidr_block = cidrsubnet(alicloud_vpc.tf-test-vpc.cidr_block, 8, each.key)
 }
