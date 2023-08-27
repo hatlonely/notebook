a372c5c590b987863ccbe2b22012d8a5e6758787
hatlonely <hatlonely@gmail.com>
2023-08-27 16:56:51 +0800

    update

--- terraform/code/alicloud/best-practise/1-ecs-web-service/cms.tf
+++ terraform/code/alicloud/best-practise/1-ecs-web-service/cms.tf
@@ -23,7 +23,7 @@ resource "alicloud_cms_monitor_group" "cms-monitor-group" {
 
 # 创建告警监控组实例
 resource "alicloud_cms_monitor_group_instances" "cms-monitor-group-instances" {
-  for_each = alicloud_instance.instances
+  for_each = {for idx, instance in alicloud_instance.instances : idx => instance}
 
   group_id = alicloud_cms_monitor_group.cms-monitor-group.id
   instances {
@@ -31,5 +31,42 @@ resource "alicloud_cms_monitor_group_instances" "cms-monitor-group-instances" {
     instance_name = each.value.instance_name
     region_id     = "cn-beijing"
     category      = "ecs"
+  }
+}
+
+# 创建告警监控组规则
+resource "alicloud_cms_group_metric_rule" "cms-group-metric-rule-cpu-total" {
+  group_id               = alicloud_cms_monitor_group.cms-monitor-group.id
+  group_metric_rule_name = "cpu-total 超过阈值"
+  rule_id                = "acs_ecs_dashboard:cpu_total"
+  category               = "ecs"
+  metric_name            = "cpu_total"
+  namespace              = "acs_ecs_dashboard"
+  period                 = 60
+  interval               = 3600
+  silence_time           = 85800
+  no_effective_interval  = "00:00-05:30"
+
+  escalations {
+    critical {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = 80
+      times               = 3
+    }
+
+    warn {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = 70
+      times               = 3
+    }
+
+    info {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = 60
+      times               = 3
+    }
   }
 }
