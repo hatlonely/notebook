c00c5cb70228c086db74278981a24539fa97ddc3
hatlonely <hatlonely@gmail.com>
2023-08-28 17:54:26 +0800

    update

--- terraform/code/aws/vpn/vpn.tf
+++ terraform/code/aws/vpn/vpn.tf
@@ -32,62 +32,62 @@ data "aws_ami" "ubuntu_22" {
 }
 
 # 创建 vpc
-resource "aws_vpc" "tf-test-vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "tf-test-subnet" {
-  vpc_id            = aws_vpc.tf-test-vpc.id
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
 }
 
-resource "aws_internet_gateway" "tf-test-internet-gateway" {
-  vpc_id = aws_vpc.tf-test-vpc.id
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_route_table" "tf-test-route-table" {
-  vpc_id = aws_vpc.tf-test-vpc.id
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tf-test-internet-gateway.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 }
 
-resource "aws_route_table_association" "tf-test-route-table-association-subnet" {
-  route_table_id = aws_route_table.tf-test-route-table.id
-  subnet_id      = aws_subnet.tf-test-subnet.id
+resource "aws_route_table_association" "route_table_association_subnet" {
+  route_table_id = aws_route_table.route-table.id
+  subnet_id      = aws_subnet.subnet.id
 }
 
 # 生成登录秘钥对
-resource "tls_private_key" "tf-test-key-pair" {
+resource "tls_private_key" "tls_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 # 保存秘钥
-resource "local_file" "tf-test-id-rsa" {
+resource "local_file" "file_id_rsa" {
   filename        = "id_rsa"
-  content         = tls_private_key.tf-test-key-pair.private_key_pem
+  content         = tls_private_key.tls_private_key.private_key_pem
   file_permission = "0600"
 }
 
-resource "local_file" "tf-test-id-rsa-pub" {
+resource "local_file" "file_id_rsa_pub" {
   filename = "id_rsa.pub"
-  content  = tls_private_key.tf-test-key-pair.public_key_openssh
+  content  = tls_private_key.tls_private_key.public_key_openssh
 }
 
 # 创建秘钥对
-resource "aws_key_pair" "tf-test-key-pair" {
+resource "aws_key_pair" "key_pair" {
   key_name   = "tf-test-key-pair"
-  public_key = tls_private_key.tf-test-key-pair.public_key_openssh
+  public_key = tls_private_key.tls_private_key.public_key_openssh
 }
 
 # 创建安全组
-resource "aws_security_group" "tf-test-security-group" {
+resource "aws_security_group" "security_group" {
   name   = "tf-test-security-group"
-  vpc_id = aws_vpc.tf-test-vpc.id
+  vpc_id = aws_vpc.vpc.id
   ingress {
     description      = "ssh"
     from_port        = 22
@@ -116,12 +116,12 @@ resource "aws_security_group" "tf-test-security-group" {
 }
 
 # 创建 EC2 实例
-resource "aws_instance" "tf-test-instance" {
+resource "aws_instance" "instance" {
   ami                         = data.aws_ami.ubuntu_22.id
   instance_type               = "t3.nano"
-  key_name                    = aws_key_pair.tf-test-key-pair.key_name
-  vpc_security_group_ids      = [aws_security_group.tf-test-security-group.id]
-  subnet_id                   = aws_subnet.tf-test-subnet.id
+  key_name                    = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids      = [aws_security_group.security_group.id]
+  subnet_id                   = aws_subnet.subnet.id
   associate_public_ip_address = true
   iam_instance_profile        = "AmazonSSMRoleForInstancesQuickSetup"
 }
@@ -139,7 +139,7 @@ resource "random_integer" "ss_port" {
 }
 
 # 创建启动脚本
-resource "aws_ssm_document" "tf-test-ssm-document-init-instance" {
+resource "aws_ssm_document" "ssm_document_init_instance" {
   name            = "tf-test-ssm-document-init-instance"
   document_type   = "Command"
   document_format = "YAML"
@@ -214,17 +214,17 @@ EOT
 }
 
 # 执行启动脚本
-resource "aws_ssm_association" "tf-test-ssm-association-init-instance" {
-  name = aws_ssm_document.tf-test-ssm-document-init-instance.name
+resource "aws_ssm_association" "ssm_association_init_instance" {
+  name = aws_ssm_document.ssm_document_init_instance.name
   targets {
     key    = "InstanceIds"
-    values = [aws_instance.tf-test-instance.id]
+    values = [aws_instance.instance.id]
   }
 }
 
 output "connection" {
   value = <<EOF
-host: ${aws_instance.tf-test-instance.public_ip}
+host: ${aws_instance.instance.public_ip}
 port: ${random_integer.ss_port.result}
 password:  ${nonsensitive(random_password.ss_password.result)}
 EOF
