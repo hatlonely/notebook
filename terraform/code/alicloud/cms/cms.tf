c9724541b921d36e3c7a962db72eb08cbb016ade
hatlonely <hatlonely@gmail.com>
2023-09-02 14:14:04 +0800

    Update cms.tf

--- terraform/code/alicloud/cms/cms.tf
+++ terraform/code/alicloud/cms/cms.tf
@@ -25,7 +25,7 @@ resource "alicloud_cms_alarm_contact" "hatlonely" {
 }
 
 # 创建告警联系组
-resource "alicloud_cms_alarm_contact_group" "tf-test-cms-alarm-contact-group" {
+resource "alicloud_cms_alarm_contact_group" "cms_alarm_contact_group" {
   alarm_contact_group_name = "tf-test-cms-alarm-contact-group"
   contacts                 = [
     alicloud_cms_alarm_contact.hatlonely.id,
@@ -33,27 +33,27 @@ resource "alicloud_cms_alarm_contact_group" "tf-test-cms-alarm-contact-group" {
 }
 
 # 创建告警监控组
-resource "alicloud_cms_monitor_group" "tf-test-cms-monitor-group" {
+resource "alicloud_cms_monitor_group" "cms_monitor_group" {
   monitor_group_name = "tf-test-cms-monitor-group"
   contact_groups     = [
-    alicloud_cms_alarm_contact_group.tf-test-cms-alarm-contact-group.id,
+    alicloud_cms_alarm_contact_group.cms_alarm_contact_group.id,
   ]
 }
 
 # 创建告警监控组实例
-resource "alicloud_cms_monitor_group_instances" "tf-test-cms-monitor-group-instances" {
-  group_id = alicloud_cms_monitor_group.tf-test-cms-monitor-group.id
+resource "alicloud_cms_monitor_group_instances" "cms_monitor_group_instances" {
+  group_id = alicloud_cms_monitor_group.cms_monitor_group.id
   instances {
-    instance_id   = alicloud_instance.tf-test-ecs.id
-    instance_name = alicloud_instance.tf-test-ecs.instance_name
+    instance_id   = alicloud_instance.instance.id
+    instance_name = alicloud_instance.instance.instance_name
     region_id     = "cn-beijing"
     category      = "ecs"
   }
 }
 
 # 创建告警监控组规则
-resource "alicloud_cms_group_metric_rule" "tf-test-cms-group-metric-rule-cpu-total" {
-  group_id               = alicloud_cms_monitor_group.tf-test-cms-monitor-group.id
+resource "alicloud_cms_group_metric_rule" "cms_group_metric_rule_cpu_total" {
+  group_id               = alicloud_cms_monitor_group.cms_monitor_group.id
   group_metric_rule_name = "cpu-total 超过阈值"
   rule_id                = "acs_ecs_dashboard:cpu_total"
   category               = "ecs"
@@ -98,7 +98,7 @@ resource "random_password" "password" {
 }
 
 # 自动获取实例类型
-data "alicloud_instance_types" "type-1c1g" {
+data "alicloud_instance_types" "instance_type_1c1g" {
   cpu_core_count = 1
   memory_size    = 1
 }
@@ -110,7 +110,7 @@ data "alicloud_images" "ubuntu_22" {
 }
 
 # 创建 VPC
-resource "alicloud_vpc" "tf-test-vpc" {
+resource "alicloud_vpc" "vpc" {
   vpc_name   = "tf-test-vpc"
   cidr_block = "172.16.0.0/16"
 }
@@ -118,35 +118,35 @@ resource "alicloud_vpc" "tf-test-vpc" {
 # 创建 VSwitch
 resource "alicloud_vswitch" "tf-test-vswitch" {
   zone_id    = "cn-beijing-b"
-  vpc_id     = alicloud_vpc.tf-test-vpc.id
+  vpc_id     = alicloud_vpc.vpc.id
   cidr_block = "172.16.0.0/24"
 }
 
 # 创建安全组，现在安全组只能创建在 VPC 内，但其规则可以是公网或内网
-resource "alicloud_security_group" "tf-test-security-group" {
+resource "alicloud_security_group" "security_group" {
   name   = "tf-test-security-group"
-  vpc_id = alicloud_vpc.tf-test-vpc.id
+  vpc_id = alicloud_vpc.vpc.id
 }
 
 # 创建安全组规则，nic_type 必须为 intranet，但其规则对公网同样有效
-resource "alicloud_security_group_rule" "tf-test-security-group-rule" {
+resource "alicloud_security_group_rule" "security_group_rule" {
   type              = "ingress"
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "1/65535"
   priority          = 1
-  security_group_id = alicloud_security_group.tf-test-security-group.id
+  security_group_id = alicloud_security_group.security_group.id
   cidr_ip           = "123.113.97.7/32"
 }
 
 # 创建一台带公网 ip 密码登录的 ECS
-resource "alicloud_instance" "tf-test-ecs" {
+resource "alicloud_instance" "instance" {
   instance_name   = "tf-test-ecs"
   image_id        = data.alicloud_images.ubuntu_22.images[0].id
-  instance_type   = data.alicloud_instance_types.type-1c1g.instance_types[0].id
+  instance_type   = data.alicloud_instance_types.instance_type_1c1g.instance_types[0].id
   security_groups = [
-    alicloud_security_group.tf-test-security-group.id,
+    alicloud_security_group.security_group.id,
   ]
   vswitch_id                 = alicloud_vswitch.tf-test-vswitch.id
   internet_max_bandwidth_out = 5
