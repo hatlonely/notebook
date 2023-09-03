a7a8a4469bd80708e1efd86766ae9b85adb2c7be
hatlonely <hatlonely@gmail.com>
2023-09-03 15:48:57 +0800

    Update sls.tf

--- terraform/code/alicloud/sls/sls.tf
+++ terraform/code/alicloud/sls/sls.tf
@@ -7,20 +7,25 @@ terraform {
   }
 }
 
+variable "name" {
+  type    = string
+  default = "tf-test-sls"
+}
+
 provider "alicloud" {
   region = "cn-beijing"
   alias  = "cn-beijing"
 }
 
 # 创建日志服务项目
-resource "alicloud_log_project" "tf-test-project" {
-  name = "tf-test-project"
+resource "alicloud_log_project" "project" {
+  name = "${var.name}-project"
 }
 
 # 创建日志服务日志库
-resource "alicloud_log_store" "tf-test-store" {
-  name                  = "tf-test-store"
-  project               = alicloud_log_project.tf-test-project.name
+resource "alicloud_log_store" "log_store" {
+  name                  = "${var.name}-store"
+  project               = alicloud_log_project.project.name
   shard_count           = 3
   auto_split            = true
   max_split_shard_count = 60
@@ -28,9 +33,9 @@ resource "alicloud_log_store" "tf-test-store" {
 }
 
 # 创建日志服务索引
-resource "alicloud_log_store_index" "tf-test-store-index" {
-  logstore = alicloud_log_store.tf-test-store.name
-  project  = alicloud_log_project.tf-test-project.name
+resource "alicloud_log_store_index" "log_store_index" {
+  logstore = alicloud_log_store.log_store.name
+  project  = alicloud_log_project.project.name
   full_text {
     case_sensitive = true
     token          = "#$^*\r\n\t"
@@ -44,19 +49,19 @@ resource "alicloud_log_store_index" "tf-test-store-index" {
 }
 
 # 创建日志服务机器组
-resource "alicloud_log_machine_group" "tf-test-machine-group" {
-  name          = "tf-test-machine-group"
-  project       = alicloud_log_project.tf-test-project.name
+resource "alicloud_log_machine_group" "log_machine_group" {
+  name          = "${var.name}-machine-group"
+  project       = alicloud_log_project.project.name
   identify_type = "userdefined"
   topic         = "tf-test-machine-group-topic"
   identify_list = ["tf-test-machine-group-identify"]
 }
 
 # 创建日志库配置
-resource "alicloud_logtail_config" "tf-test-logtail-config" {
-  name         = "tf-test-logtail-config"
-  project      = alicloud_log_project.tf-test-project.name
-  logstore     = alicloud_log_store.tf-test-store.name
+resource "alicloud_logtail_config" "logtail_config" {
+  name         = "${var.name}-logtail-config"
+  project      = alicloud_log_project.project.name
+  logstore     = alicloud_log_store.log_store.name
   input_type   = "file"
   output_type  = "LogService"
   input_detail = <<EOF
@@ -74,8 +79,8 @@ EOF
 }
 
 # 关联日志库配置
-resource "alicloud_logtail_attachment" "tf-test-logtail-attachment" {
-  logtail_config_name = alicloud_logtail_config.tf-test-logtail-config.name
-  machine_group_name  = alicloud_log_machine_group.tf-test-machine-group.name
-  project             = alicloud_log_project.tf-test-project.name
+resource "alicloud_logtail_attachment" "logtail_attachment" {
+  logtail_config_name = alicloud_logtail_config.logtail_config.name
+  machine_group_name  = alicloud_log_machine_group.log_machine_group.name
+  project             = alicloud_log_project.project.name
 }
