e6553362dae9412bc345fb414afa70aee4919978
hatlonely <hatlonely@gmail.com>
2023-09-03 14:14:48 +0800

    Update ecs.tf

--- terraform/code/alicloud/ecs/ecs.tf
+++ terraform/code/alicloud/ecs/ecs.tf
@@ -30,7 +30,7 @@ resource "random_password" "password" {
 }
 
 # 自动获取实例类型
-data "alicloud_instance_types" "type-1c1g" {
+data "alicloud_instance_types" "instance_types_1c1g" {
   cpu_core_count = 1
   memory_size    = 1
 }
@@ -42,7 +42,7 @@ data "alicloud_images" "ubuntu_22" {
 }
 
 # 创建 VPC
-resource "alicloud_vpc" "tf-test-vpc" {
+resource "alicloud_vpc" "vpc" {
   vpc_name   = "tf-test-vpc"
   cidr_block = "172.16.0.0/16"
 }
@@ -50,34 +50,34 @@ resource "alicloud_vpc" "tf-test-vpc" {
 # 创建 VSwitch
 resource "alicloud_vswitch" "tf-test-vswitch" {
   zone_id    = "cn-beijing-b"
-  vpc_id     = alicloud_vpc.tf-test-vpc.id
+  vpc_id     = alicloud_vpc.vpc.id
   cidr_block = "172.16.0.0/24"
 }
 
 # 创建安全组，现在安全组只能创建在 VPC 内，但其规则可以是公网或内网
-resource "alicloud_security_group" "tf-test-security-group" {
+resource "alicloud_security_group" "security_group" {
   name   = "tf-test-security-group"
-  vpc_id = alicloud_vpc.tf-test-vpc.id
+  vpc_id = alicloud_vpc.vpc.id
 }
 
 # 创建安全组规则，nic_type 必须为 intranet，但其规则对公网同样有效
-resource "alicloud_security_group_rule" "tf-test-security-group-rule" {
+resource "alicloud_security_group_rule" "security_group_rule" {
   type              = "ingress"
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "1/65535"
   priority          = 1
-  security_group_id = alicloud_security_group.tf-test-security-group.id
+  security_group_id = alicloud_security_group.security_group.id
   cidr_ip           = "123.113.97.7/32"
 }
 
 # 创建一台带公网 ip 密码登录的 ECS
-resource "alicloud_instance" "tf-test-ecs" {
+resource "alicloud_instance" "instance" {
   image_id        = data.alicloud_images.ubuntu_22.images[0].id
-  instance_type   = data.alicloud_instance_types.type-1c1g.instance_types[0].id
+  instance_type   = data.alicloud_instance_types.instance_types_1c1g.instance_types[0].id
   security_groups = [
-    alicloud_security_group.tf-test-security-group.id,
+    alicloud_security_group.security_group.id,
   ]
   vswitch_id                 = alicloud_vswitch.tf-test-vswitch.id
   internet_max_bandwidth_out = 5
@@ -88,7 +88,7 @@ resource "alicloud_instance" "tf-test-ecs" {
 
 output "connection" {
   value = <<EOF
-  ssh root@${alicloud_instance.tf-test-ecs.public_ip}
-  ${nonsensitive(alicloud_instance.tf-test-ecs.password)}
+  ssh root@${alicloud_instance.instance.public_ip}
+  ${nonsensitive(alicloud_instance.instance.password)}
 EOF
 }
