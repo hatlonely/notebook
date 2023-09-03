77bbc8385bced148838851f953f21f12d623bb8c
hatlonely <hatlonely@gmail.com>
2023-09-03 14:16:24 +0800

    Update eci.tf

--- terraform/code/alicloud/eci/eci.tf
+++ terraform/code/alicloud/eci/eci.tf
@@ -13,40 +13,40 @@ provider "alicloud" {
 }
 
 # 创建 VPC
-resource "alicloud_vpc" "tf-test-vpc" {
+resource "alicloud_vpc" "vpc" {
   vpc_name   = "tf-test-vpc"
   cidr_block = "172.16.0.0/16"
 }
 
 # 创建 VSwitch
-resource "alicloud_vswitch" "tf-test-vswitch" {
+resource "alicloud_vswitch" "vswitch" {
   zone_id    = "cn-beijing-i"
-  vpc_id     = alicloud_vpc.tf-test-vpc.id
+  vpc_id     = alicloud_vpc.vpc.id
   cidr_block = "172.16.0.0/24"
 }
 
 # 创建安全组，现在安全组只能创建在 VPC 内，但其规则可以是公网或内网
-resource "alicloud_security_group" "tf-test-security-group" {
+resource "alicloud_security_group" "security_group" {
   name   = "tf-test-security-group"
-  vpc_id = alicloud_vpc.tf-test-vpc.id
+  vpc_id = alicloud_vpc.vpc.id
 }
 
 # 创建安全组规则，nic_type 必须为 intranet，但其规则对公网同样有效
-resource "alicloud_security_group_rule" "tf-test-security-group-rule" {
+resource "alicloud_security_group_rule" "security_group_rule" {
   type              = "ingress"
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "1/65535"
   priority          = 1
-  security_group_id = alicloud_security_group.tf-test-security-group.id
+  security_group_id = alicloud_security_group.security_group.id
   cidr_ip           = "123.113.97.7/32"
 }
 
-resource "alicloud_eci_container_group" "tf-test-eci-container-group" {
+resource "alicloud_eci_container_group" "eci_container_group" {
   container_group_name = "tf-test-eci-container-group"
-  security_group_id    = alicloud_security_group.tf-test-security-group.id
-  vswitch_id           = alicloud_vswitch.tf-test-vswitch.id
+  security_group_id    = alicloud_security_group.security_group.id
+  vswitch_id           = alicloud_vswitch.vswitch.id
   restart_policy       = "OnFailure"
   cpu                  = 1
   memory               = 2
@@ -65,5 +65,5 @@ resource "alicloud_eci_container_group" "tf-test-eci-container-group" {
 }
 
 output "connection" {
-  value = "curl http://${alicloud_eci_container_group.tf-test-eci-container-group.internet_ip}"
+  value = "curl http://${alicloud_eci_container_group.eci_container_group.internet_ip}"
 }
