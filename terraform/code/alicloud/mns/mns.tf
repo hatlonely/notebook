abd50c5f64ce66e4744899879d433271c9f2e9c5
hatlonely <hatlonely@gmail.com>
2023-09-03 15:21:02 +0800

    Update mns.tf

--- terraform/code/alicloud/mns/mns.tf
+++ terraform/code/alicloud/mns/mns.tf
@@ -9,6 +9,11 @@ terraform {
   }
 }
 
+variable "name" {
+  type    = string
+  default = "tf-test-mns"
+}
+
 provider "alicloud" {
   region = "cn-beijing"
   alias  = "cn-beijing"
@@ -20,13 +25,13 @@ data "alicloud_regions" "regions" {
 
 data "alicloud_account" "account" {}
 
-resource "alicloud_mns_topic" "test-topic" {
-  name                 = "tf-test-mns-topic"
+resource "alicloud_mns_topic" "mns_topic" {
+  name                 = "${var.name}-topic"
   maximum_message_size = 65536
   logging_enabled      = false
 }
 
-resource "alicloud_mns_queue" "test-queue" {
+resource "alicloud_mns_queue" "mns_queue" {
   name                     = "tf-test-mns-queue"
   delay_seconds            = 0
   maximum_message_size     = 65536
@@ -35,17 +40,17 @@ resource "alicloud_mns_queue" "test-queue" {
   polling_wait_seconds     = 0
 }
 
-resource "alicloud_mns_topic_subscription" "test-subscription" {
-  topic_name            = alicloud_mns_topic.test-topic.name
-  name                  = "tf-test-mns-subscription"
-  endpoint              = "acs:mns:${data.alicloud_regions.regions.regions[0].id}:${data.alicloud_account.account.id}:queues/${alicloud_mns_queue.test-queue.name}"
+resource "alicloud_mns_topic_subscription" "mns_topic_subscription_queue" {
+  topic_name            = alicloud_mns_topic.mns_topic.name
+  name                  = "${var.name}-subscription-queue"
+  endpoint              = "acs:mns:${data.alicloud_regions.regions.regions[0].id}:${data.alicloud_account.account.id}:queues/${alicloud_mns_queue.mns_queue.name}"
   notify_strategy       = "BACKOFF_RETRY"
   notify_content_format = "JSON"
 }
 
 output "topic" {
   value = {
-    name     = alicloud_mns_topic.test-topic.name
+    name     = alicloud_mns_topic.mns_topic.name
     endpoint = "http://${data.alicloud_account.account.id}.mns.${data.alicloud_regions.regions.regions[0].id}.aliyuncs.com/"
   }
 }
