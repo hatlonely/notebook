b67d4def55762111b254cff86fb72484f776ccde
hatlonely <hatlonely@gmail.com>
2023-09-08 21:28:06 +0800

    update

---
+++ python/syntax/test_file.py
@@ -0,0 +1,17 @@
+import os
+
+
+def test_list_directory():
+    for i in os.listdir("."):
+        if os.path.isdir(i):
+            print(f"{i} is directory")
+        else:
+            print(f"{i} is file")
+
+
+def test_walk():
+    for root, dirs, files in os.walk("./"):
+        for i in ([os.path.join(root, f) for f in files]):
+            print(i)
+        for i in ([os.path.join(root, d) for d in dirs]):
+            print(i)
