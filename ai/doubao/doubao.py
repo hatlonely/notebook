7946affba88ff7e5f0b05cbbddaaccfed654e0bb
hatlonely <hatlonely@foxmail.com>
2025-02-15 20:55:57 +0800

    update

---
+++ ai/doubao/doubao.py
@@ -0,0 +1,16 @@
+from volcenginesdkarkruntime import Ark
+
+client = Ark(
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+# Non-streaming:
+print("----- standard request -----")
+completion = client.chat.completions.create(
+    model="ep-20250215183442-dwqt8",
+    messages=[
+        {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
+        {"role": "user", "content": "常见的十字花科植物有哪些？"},
+    ],
+)
+print(completion.choices[0].message.content)
